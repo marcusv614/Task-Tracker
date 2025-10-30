32826d17bce4e7b45419fed94b6d8486264a0f1a
Marcus Pinheiro <marcus.pinheiro@super.ufam.edu.br>
2025-10-29 21:07:36 -0400

    v(2.1)
    Implementação das funções:
    -Alterar Tarefas:(nomes,data,descrição e status)

--- src/tasktracker/menu/Menu.java
+++ src/tasktracker/menu/Menu.java
@@ -10,6 +10,13 @@ public final class Menu {
     ArrayList<Tarefa> listaTaf = new ArrayList<>();
     ArrayList<Eventos> listaEv = new ArrayList<>();
 
+    public void subMenu1() {
+        System.out.println("""
+                           [0] Voltar
+                           [1] Alterar Dados
+                           """);
+    }
+
     public void mostrarListaTaf() {
         for (int i = 0; i < listaTaf.size(); i++) {
             Tarefa t = listaTaf.get(i);
@@ -73,12 +80,92 @@ public final class Menu {
                 }
                 case 4 -> {
                     if (listaTaf != null && !listaTaf.isEmpty()) {
-                        boolean sairSubMenu = false;
                         mostrarListaTaf();
-                                    } else {
-                                        System.out.println("Índice inválido!\n");
-                                    }
+                        subMenu1();
+                        while (true) {
+                            
+                            if (!sc.hasNextInt()) {
+                                sc.nextLine();
+                                continue;
+                            }
+
+                            int resp2 = sc.nextInt();
+                            sc.nextLine();
+
+                            if (resp2 == 0) {
+                                break;
+                            } else if (resp2 == 1) {
+                                System.out.println("Digite o índice da tarefa a ser editada. \n");
+                                
+                                if (!sc.hasNextInt()) {
+                                    sc.nextLine();
+                                    continue;
                                 }
+
+                                int resp3 = sc.nextInt();
+                                sc.nextLine();
+                                
+                                if (resp3 < 0 || resp3 >= listaTaf.size()) {
+                                    System.out.println("Índice fora do intervalo. Tente novamente.\n");
+                                    continue;
+                                }
+
+                                Tarefa t = listaTaf.get(resp3);
+                                System.out.println("===== TAREFA [" + resp3 + "] =====");
+                                System.out.println("Data: " + t.getData());
+                                System.out.println("Nome da Tarefa: " + t.getNomeEvento());
+                                System.out.println("Descrição: " + t.getDesc());
+                                System.out.println("Situação: " + t.getSituacao());
+                                System.out.println("=============================\n");
+                                System.out.println("""
+                                                   Editar:
+                                                   [1] Data
+                                                   [2] Nome
+                                                   [3] Descrição
+                                                   [4] Status
+                                                   """);
+                                
+                                if (!sc.hasNextInt()) {
+                                    sc.nextLine();
+                                    continue;
+                                }
+ 
+                                int resp4 = sc.nextInt();
+                                sc.nextLine();
+
+                                switch (resp4) {
+                                    case 1 -> {
+                                        System.out.println("Digite a nova data: \n");
+                                        String novaData = sc.nextLine();
+                                        listaTaf.get(resp3).setData(novaData);
+                                    }
+                                    case 2 -> {
+                                        System.out.println("Digite o novo nome: \n");
+                                        String novoNome = sc.nextLine();
+                                        listaTaf.get(resp3).setNomeEvento(novoNome);
+                                    }
+                                    case 3 -> {
+                                        System.out.println("Digite a nova descrição: \n");
+                                        String novaDesc = sc.nextLine();
+                                        listaTaf.get(resp3).setDesc(novaDesc);
+                                    }
+                                    case 4 -> {
+                                        System.out.println("Digite o novo status [P] [C]");
+                                        String novoStatus = sc.nextLine();
+                                        listaTaf.get(resp3).setSituacao(novoStatus);
+                                    }
+                                    default ->
+                                        System.out.println("valor inválido");
+                                }
+                                
+                                System.out.println("Tarefa atualizada !");
+                                break;
+                            }
+                        }
+                    } else {
+                        System.out.println("Nenhuma tarefa cadastrada!\n");
+                    }
+                }
                 default -> {
                     System.out.println("Valor inválido!\n");
                 }
